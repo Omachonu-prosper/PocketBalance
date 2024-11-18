1f1e63c13c07946eb198481878106939b838c2f3
Omachonu Prosper <omachonucodes@gmail.com>
2024-11-18 22:20:32 +0100

    Create the asset cards

---
+++ src/lib/components/AssetCard.svelte
@@ -0,0 +1,34 @@
+<script>
+    import Icon from '@iconify/svelte';
+
+    let { 
+        type, amount, button1, button2, currency = '₦',
+        cardColour, buttonColour
+    } = $props()
+    let showing = $state(false)
+
+    const toggleAmount = (e) => {
+        showing = !showing
+    }
+</script>
+
+
+<div class="{cardColour} rounded-xl relative h-36 mt-8">
+    <div class="px-3 pt-3">
+        <p class="text-white text-xs px-1 border inline-block rounded-md">{type}</p>
+        <h2 class="text-white text-2xl mt-1">
+            { showing ? currency + amount : '***' }
+            <Icon icon={showing ? 'solar:eye-bold' : 'solar:eye-closed-bold'} class="inline-block text-2xl pl-1 pb-1"
+                onclick={toggleAmount}/>
+        </h2>
+    </div>
+    
+    <div class="flex justify-stretch absolute bottom-2 w-full p-2">
+        <button class="text-white flex-auto mx-1 p-1 rounded-xl {buttonColour}" type="button">
+           {button1} <Icon icon="solar:course-up-broken" class="inline-block text-2xl pl-1 pb-1" />
+        </button>
+        <button class="text-white flex-auto mx-1 p-1 rounded-xl {buttonColour}" type="button">
+            {button2} <Icon icon="solar:course-down-broken" class="inline-block text-2xl pl-1 pb-1" />
+        </button>
+    </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,33 @@
-<h1 class="text-3xl font-bold underline">
-    Hello world!
-</h1>
+<script>
+    import Icon from '@iconify/svelte';
+    import AssetCard from '$lib/components/AssetCard.svelte';
+</script>
 
-<style lang="postcss">
-    :global(html) {
-        background-color: theme(colors.gray.100);
-    }
-</style>
+<svelte:head>
+  <title>Dashboard | PocketBalance</title>
+</svelte:head>
+
+<section class="max-w-md container mx-auto p-3">
+    <header class="flex pt-3 items-center">
+        <h2 class="text-xl flex-auto">Hello User</h2>
+
+        <Icon icon="solar:moon-stars-bold" class="text-3xl" />
+    </header>
+
+    <main class="mt-5">
+        <AssetCard type='Funds' amount="30,000" button1="Add funds" button2="Reduce funds" 
+            cardColour='bg-blue-700', buttonColour='bg-blue-900' />
+
+        <AssetCard type='Debts' amount="30,000" button1="Increase debt" button2="Reduce debt"
+            cardColour='bg-red-500' buttonColour='bg-red-700' />
+
+        <AssetCard type='Savings' amount="30,000" button1="Save" button2="Withdraw"
+            cardColour='bg-neutral-700' buttonColour='bg-neutral-900' currency='$' />
+    </main>
+
+    <!-- <footer>
+        <div>
+            
+        </div>
+    </footer> -->
+</section>
