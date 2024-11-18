2455344ecbba1af4abe869e87fee6c3c7514ddf0
Omachonu Prosper <omachonucodes@gmail.com>
2024-11-19 00:46:12 +0100

    Create the transaction history page

---
+++ src/lib/components/Transaction.svelte
@@ -0,0 +1,18 @@
+<script>
+    import Icon from '@iconify/svelte';
+
+    let { title, date, badgeColour, amount, currency = '₦' } = $props()
+</script>
+
+<div class="flex items-center mb-5">
+    <Icon icon="grommet-icons:transaction" class="{badgeColour} rounded-full size-10 p-2 text-white"/>
+    
+    <div class="flex-auto mx-2">
+        <div class="text-sm">{title}</div>
+        <div class="text-xs">{date}</div>
+    </div>
+    
+    <div class="text-sm">
+        {currency}{amount}
+    </div>
+</div>
--- src/routes/history/+page.svelte
+++ src/routes/history/+page.svelte
@@ -1,6 +1,6 @@
 <script>
-    import Icon from '@iconify/svelte';
     import Footer from '$lib/components/Footer.svelte';
+    import Transaction from '$lib/components/Transaction.svelte';
 </script>
 
 <svelte:head>
@@ -12,8 +12,13 @@
         <h2 class="text-md text-center">Transaction history</h2>
     </header>
 
-    <main class="mt-5">
-
+    <main class="mt-5 mb-20">
+        <Transaction title="Added to funds" date="Tue Nov 19 2024 11:20:23" amount="40,000" badgeColour="bg-blue-400" />
+        <Transaction title="Added to debts" date="Tue Nov 16 2024 11:20:23" amount="20,000" badgeColour="bg-red-400" />
+        <Transaction title="Added to savings" date="Tue Nov 19 2024 11:20:23" amount="4,000" badgeColour="bg-neutral-400" />
+        <Transaction title="Added to funds" date="Tue Nov 19 2024 11:20:23" amount="400" badgeColour="bg-blue-400" />
+        <Transaction title="Added to debts" date="Tue Nov 19 2024 11:20:23" amount="230,000" badgeColour="bg-red-400" />
+        <Transaction title="Removed from savings" date="Tue Nov 19 2024 11:20:23" amount="40,000" badgeColour="bg-neutral-400" currency='$'/>
     </main>
 </section>
 
