e7677101977dd95e27f368e5db51049c5da75e54
Omachonu Prosper <omachonucodes@gmail.com>
2024-11-19 01:00:40 +0100

    Create the account page

--- src/routes/account/+page.svelte
+++ src/routes/account/+page.svelte
@@ -13,7 +13,17 @@
     </header>
 
     <main class="mt-5">
+        <div class="flex justify-center">
+            <Icon icon="ic:round-account-circle" class="size-32 text-neutral-400 mb-5" />
+        </div>
 
+        <div class="h-80 rounded-xl bg-neutral-200">
+
+        </div>
+
+        <a href="/login" class="border border-red-400 w-full inline-block py-3 text-center mt-5 rounded-xl text-red-600 hover:text-white hover:bg-red-600 focus:text-white focus:bg-red-600 focus:outline-none transition-all">
+            Logout
+        </a>        
     </main>
 </section>
 
