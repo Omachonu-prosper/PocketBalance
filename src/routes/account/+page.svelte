90428a81a2fef891dabeb8a800eca304ad2973de
Omachonu Prosper <omachonucodes@gmail.com>
2024-11-19 20:51:01 +0100

    Add account data

--- src/routes/account/+page.svelte
+++ src/routes/account/+page.svelte
@@ -17,8 +17,21 @@
             <Icon icon="ic:round-account-circle" class="size-32 text-neutral-400 mb-5" />
         </div>
 
-        <div class="h-80 rounded-xl bg-neutral-200">
+        <div class="rounded-xl bg-neutral-200 p-3 pt-5">
+            <div class="pb-5">
+                <div class="text-sm">- full name -</div>
+                <div class="text-lg">John Doe</div>
+            </div>
 
+            <div class="pb-5">
+                <div class="text-sm">- email -</div>
+                <div class="text-lg">johndoe@example.com</div>
+            </div>
+
+            <div class="pb-5">
+                <div class="text-sm">- joined -</div>
+                <div class="text-lg">Mon Nov 18 2024</div>
+            </div>
         </div>
 
         <a href="/login" class="border border-red-400 w-full inline-block py-3 text-center mt-5 rounded-xl text-red-600 hover:text-white hover:bg-red-600 focus:text-white focus:bg-red-600 focus:outline-none transition-all">
